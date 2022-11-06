75c0ece8db801e47448d538174a3285b8a619341
tompng <tomoyapenguin@gmail.com>
2022-11-06 18:56:09 +0900

    update indent sample

--- sample.rb
+++ sample.rb
@@ -1,14 +1,21 @@
 require 'irb'
 require_relative './trex'
+require_relative './ruby_lex_patch'
+if ARGV.any?
+  input = ARGV.map{ File.read _1 }.join($/)
+else
+  input = File.read('./trex.rb') + File.read(__FILE__)
+end
 
-code = File.read('./trex.rb') + File.read(__FILE__)
-code.gsub!(/\n +/, "\n")
-tokens = RubyLex.ripper_lex_without_warning code
+tokens = RubyLex.ripper_lex_without_warning input.gsub(/\n +/, "\n")
 
 code = ''
+rubylex = Class.new{include RubyLexPatch}.new
 TRex.parse_line(tokens).first.each do |ltokens, prev_opens, _next_opens, min_depths|
-  level = prev_opens.take(min_depths).size
-  indent = '  ' * level
+  open_tokens = prev_opens.map(&:first)
+  min_depth_level, = rubylex.calc_nesting_depth open_tokens.take(min_depths)
+  prev_level, = rubylex.calc_nesting_depth open_tokens
+  indent = '  ' * [min_depth_level, prev_level].min # need comparing for heredoc_end
   if ltokens.first&.first&.event == :on_sp
     code << indent + ltokens.drop(1).map(&:last).join
   else
