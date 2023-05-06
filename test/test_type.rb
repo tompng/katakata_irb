bde14667faa13fd1ea8b5d7d73519a22650b8ea4
tompng <tomoyapenguin@gmail.com>
2023-05-07 02:23:36 +0900

    Add type_from_object test

--- test/test_type.rb
+++ test/test_type.rb
@@ -19,4 +19,26 @@ class TestType < Minitest::Test
     assert_equal 'Proc', KatakataIrb::Types::PROC.inspect
     assert_equal 'Array.itself', KatakataIrb::Types::SingletonType.new(Array).inspect
   end
+
+  def test_type_from_object
+    obj = Object.new
+    arr = [1, 'a']
+    hash = { 'key' => :value }
+    int_type = KatakataIrb::Types.type_from_object 1
+    obj_type = KatakataIrb::Types.type_from_object obj
+    arr_type = KatakataIrb::Types.type_from_object arr
+    hash_type = KatakataIrb::Types.type_from_object hash
+
+    assert_equal Integer, int_type.klass
+    # Use singleton_class to autocomplete singleton methods
+    assert_equal obj.singleton_class, obj_type.klass
+    # Array and Hash are special
+    assert_equal Array, arr_type.klass
+    assert_equal Hash, hash_type.klass
+    assert_equal 'Object', obj_type.inspect
+    assert_equal 'Array[Elem: Integer | String]', arr_type.inspect
+    assert_equal 'Hash[K: String, V: Symbol]', hash_type.inspect
+    assert_equal 'Array.itself', KatakataIrb::Types.type_from_object(Array).inspect
+    assert_equal 'KatakataIrb.itself', KatakataIrb::Types.type_from_object(KatakataIrb).inspect
+  end
 end
