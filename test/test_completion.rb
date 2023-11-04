ce62a7993303ca96898b1d076a9378c5a2f4eb0a
tompng <tomoyapenguin@gmail.com>
2023-11-05 01:09:03 +0900

    skip test needs attached_object in ruby <= 3.1

--- test/test_completion.rb
+++ test/test_completion.rb
@@ -85,10 +85,12 @@ class TestCompletor < Minitest::Test
     bind = binding
     assert_completion('bo.f', binding: bind, include: 'foo')
     assert_completion('def bo.baz; self.', binding: bind, include: 'foo')
-    assert_completion('def bo.baz; @', binding: bind, include: '@bar')
-    assert_completion('def bo.baz; @bar.', binding: bind, include: 'abs')
     bo_self_bind = bo.instance_eval { Kernel.binding }
     assert_completion('@', binding: bo_self_bind, include: '@bar')
     assert_completion('@bar.', binding: bo_self_bind, include: 'abs')
+    if Class.method_defined? :attached_object
+      assert_completion('def bo.baz; @', binding: bind, include: '@bar')
+      assert_completion('def bo.baz; @bar.', binding: bind, include: 'abs')
+    end
   end
 end
